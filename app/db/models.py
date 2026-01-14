8e773af99fa99150b9961e7bcb18f94a7a7e05aa
jerald <jeraldfrancis.bagsic@dict.gov.ph>
2026-01-14 08:42:17 +0800

    Remove unused application files and directories

--- app/db/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float
-from .database import Base
-
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    quantity = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
